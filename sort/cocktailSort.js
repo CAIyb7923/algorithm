8b10049d7dcc41e5a9c7306493814b8007bd01d2
caiyongbo <15651987198@163.com>
2020-12-28 21:23:41 +0800

    新增快速排序的js实现

--- sort/cocktailSort.js
+++ sort/cocktailSort.js
@@ -3,6 +3,7 @@
  * 优点：在大部分元素已经有序的情况下，能够减少排序的回合数
  * 缺点：增加了代码量
  * @param {array} arr 待排序数组
+ * @returns {array}
  */
 function cocktailSort(arr) {
   for (let i = 0; i < (arr.length / 2); i++) {
---
+++ sort/quickSort.js
@@ -0,0 +1,66 @@
+/**
+ * 快速排序
+ * 非递归实现
+ */
+function quickSort(arr, startIndex, endIndex) {
+  const quickSortStack = [];
+  const rootParam = new Map();
+  rootParam.set('startIndex', startIndex);
+  rootParam.set('endIndex', endIndex);
+  quickSortStack.push(rootParam);
+
+  // 循环结束条件：栈为空时
+  while(quickSortStack.length !== 0) {
+    // 栈顶元素出栈，得到起止下标
+    const param = quickSortStack.pop();
+
+    // 得到基准元素位置
+    const pivotIndex = partition(
+      arr,
+      param.get('startIndex'),
+      param.get('endIndex')
+    );
+
+    // 根据基准元素分成两部分，把每一部分的起止下标入栈
+    if (param.get('startIndex') < (pivotIndex - 1)) {
+      const leftParam = new Map();
+      leftParam.set('startIndex', param.get('startIndex'));
+      leftParam.set('endIndex', pivotIndex - 1);
+      quickSortStack.push(leftParam);
+    }
+
+    if ((pivotIndex + 1) < param.get('endIndex')) {
+      const rightParam = new Map();
+      rightParam.set('startIndex', pivotIndex + 1);
+      rightParam.set('endIndex', param.get('endIndex'));
+      quickSortStack.push(rightParam);
+    }
+  }
+
+  return arr;
+}
+
+/**
+ * 分治（单边循环法）
+ * @param {array} arr 待排序数组
+ * @param {number} startIndex 起始下标
+ * @param {number} endIndex 结束下标
+ * @returns {number}
+ */
+function partition(arr, startIndex, endIndex) {
+  // 取第一个元素作为基准元素
+  const pivot = arr[startIndex];
+  let mark = startIndex;
+
+  for (let i = (startIndex + 1); i <= endIndex; i++) {
+    if (arr[i] < pivot) {
+      mark ++;
+      [arr[mark], arr[i]] = [arr[i], arr[mark]];
+    }
+  }
+
+  arr[startIndex] = arr[mark];
+  arr[mark] = pivot;
+
+  return mark;
+}
